3e38b80027e04408ca3dbd18634ebbaf264fcd6c
NazAbdul <95302790+NazAbdul@users.noreply.github.com>
2022-06-07 00:21:15 +0100

    countdown timer repositeries

---
+++ script.js
@@ -0,0 +1,27 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secsEl = document.getElementById("secs");
+
+const newBeginnings = "1 Jan 2023";
+
+function countdown() {
+  const newBeginningsDate = new Date(newBeginnings);
+  const currentDate = new Date();
+
+  const totalSeconds = (newBeginningsDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const mins = Math.floor(totalSeconds / 60) % 60;
+  const secs = Math.floor(totalSeconds) % 60;
+
+  daysEl.innerHTML = formatTime(days);
+  hoursEl.innerHTML = formatTime(hours);
+  minsEl.innerHTML = formatTime(mins);
+  secsEl.innerHTML = formatTime(secs);
+}
+
+countdown();
+
+setInterval(countdown, 1000);
